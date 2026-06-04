7b6a0d856713e310b5a8c24c4ec71718a8dbaefb
FranklinNgaleu <franklinngaleu24@gmail.com>
2026-06-04 17:34:30 +0200

    fix all

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // En développement, utilisez l'URL complète
-const API_URL = 'http://localhost:8000';
+// const API_URL = 'http://localhost:8000';
+const API_URL = 'http://127.0.0.1:8000';
 
 const api = axios.create({
   baseURL: API_URL,
